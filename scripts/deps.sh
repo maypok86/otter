23a97a1c66648144667680fce6ce678c0507ce68
maypok86 <alex.mayshev.86@gmail.com>
2024-08-29 11:07:00 +0300

    [Chore] Use copyloopvar linter instead of exportloopref

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,7 +3,7 @@
 set -e
 
 (which golangci-lint > /dev/null) || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh |
-bash -s -- -b "$(go env GOPATH)"/bin v1.55.2
+bash -s -- -b "$(go env GOPATH)"/bin v1.60.3
 
 go install github.com/daixiang0/gci@latest
 GO111MODULE=on go install mvdan.cc/gofumpt@latest
