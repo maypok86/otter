e884250cf7238253477acd6e178bdbed50b9e96e
maypok86 <alex.mayshev.86@gmail.com>
2025-06-11 19:09:51 +0300

    chore: add tests for the сause of deletion

---
+++ deletion_test.go
@@ -0,0 +1,75 @@
+package otter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeletionCause_String(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		cause DeletionCause
+		want  string
+	}{
+		{
+			cause: CauseInvalidation,
+			want:  "Invalidation",
+		},
+		{
+			cause: CauseOverflow,
+			want:  "Overflow",
+		},
+		{
+			cause: CauseReplacement,
+			want:  "Replacement",
+		},
+		{
+			cause: CauseExpiration,
+			want:  "Expiration",
+		},
+		{
+			cause: causeUnknown,
+			want:  "<unknown otter.DeletionCause>",
+		},
+	}
+
+	for _, tt := range tests {
+		require.Equal(t, tt.want, tt.cause.String())
+	}
+}
+
+func TestDeletionCause_IsEviction(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		cause DeletionCause
+		want  bool
+	}{
+		{
+			cause: CauseInvalidation,
+			want:  false,
+		},
+		{
+			cause: CauseOverflow,
+			want:  true,
+		},
+		{
+			cause: CauseReplacement,
+			want:  false,
+		},
+		{
+			cause: CauseExpiration,
+			want:  true,
+		},
+		{
+			cause: causeUnknown,
+			want:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		require.Equal(t, tt.want, tt.cause.IsEviction())
+	}
+}
